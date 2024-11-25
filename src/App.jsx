91440af8f374e1608e587f8147b0ea95dd72dcc7
Aakash Pal <akashpal1482@gmail.com>
2024-11-25 11:05:18 +0530

    a small bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react"
-import InputBox from "./components/InputBox"
-import TodoList from "./components/TodoList"
 import { Todoprovider } from "./contexts";
 import { TodoForm, TodoItem } from "./components";
 
